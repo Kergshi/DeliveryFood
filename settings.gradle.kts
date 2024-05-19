a3363bb2fd38c153b1242fbb84bd47daed9742be
Fedor Shtanko <atraxlxxxl@gmail.com>
2024-05-19 21:43:11 +0300

    new2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,2 +1,11 @@
 rootProject.name = "DeliveryFood"
 
+includeBuild("api-gateway")
+includeBuild("auth-service")
+includeBuild("delivery-service")
+includeBuild("eureka-server")
+includeBuild("food-service")
+includeBuild("notification-service")
+includeBuild("order-service")
+includeBuild("payment-service")
+
